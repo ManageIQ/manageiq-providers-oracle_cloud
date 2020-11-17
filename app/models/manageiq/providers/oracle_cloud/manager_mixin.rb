36b7a494801b987daf430edd8c356095d5260367
Adam Grare <agrare@redhat.com>
2020-11-17 10:24:38 -0500

    Connect and verify_credentials using OCI

--- app/models/manageiq/providers/oracle_cloud/manager_mixin.rb
+++ app/models/manageiq/providers/oracle_cloud/manager_mixin.rb
@@ -6,12 +6,28 @@ module ManageIQ::Providers::OracleCloud::ManagerMixin
   end
 
   def connect(options = {})
-    self.class.raw_connect(authentication_userid(options[:auth_type]), authentication_password(options[:auth_type]), hostname, api)
+    authentication = authentication_best_fit(options[:auth_type])
+    config = self.class.raw_connect(
+      authentication.userid,
+      authentication.service_account,
+      authentication.auth_key,
+      authentication.public_key,
+      provider_region
+    )
+
+    if options[:service]
+      api_client_klass = "OCI::#{options[:service]}".safe_constantize
+      raise ArgumentError, _("Invalid service") if api_client_klass.nil?
+
+      api_client_klass.new(:config => config)
+    else
+      config
+    end
   end
 
-  def verify_credentials(_auth_type = nil, _options = {})
+  def verify_credentials(auth_type = nil, _options = {})
     begin
-      connect
+      connect(:service => "Identity::IdentityClient").get_user(authentication_userid(auth_type))
     rescue => err
       raise MiqException::MiqInvalidCredentialsError, err.message
     end
@@ -29,15 +45,19 @@ module ManageIQ::Providers::OracleCloud::ManagerMixin
     def raw_connect(user, tenant, private_key, public_key, region)
       require "oci"
 
+      # Strip out any "----- BEGIN/END PUBLIC KEY -----" lines
       public_key = public_key.split("\n").delete_if { |part| part.start_with?("-----") }.join("\n")
+      # Build a key fingerprint e.g. aa:bb:cc:dd:ee...
       fingerprint = Digest::MD5.hexdigest(Base64.decode64(public_key)).scan(/../).join(":")
 
       config = OCI::Config.new
+
       config.user        = user
       config.tenancy     = tenant
-      config.key_content = private_key
+      config.key_content = ManageIQ::Password.try_decrypt(private_key)
       config.fingerprint = fingerprint
       config.region      = region
+
       config
     end
   end
