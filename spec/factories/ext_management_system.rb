8db599008d431f68fd01eed6e5788eedfa396c9a
Adam Grare <agrare@redhat.com>
2021-03-01 15:46:42 -0500

    Add vcr default secrets

--- spec/factories/ext_management_system.rb
+++ spec/factories/ext_management_system.rb
@@ -7,13 +7,6 @@ FactoryBot.define do
     uid_ems { Rails.application.secrets.oracle_cloud[:tenant_id] }
 
     after(:create) do |ems|
-      # These are all bogus values but Rails Secrets won't work because the replaced
-      # values don't pass the local sdk client validation.  E.g. ORACLE_CLOUD_USER_ID
-      # fails the user id format validation.
-      #
-      # To re-record the cassette replace these values with real ones and add
-      # config.define_cassette_placeholder("ocid1.user.oc1..aaaaaaaa") { "real value" }
-      # to spec/spec_helper.rb
       user_id     = Rails.application.secrets.oracle_cloud[:user_id]
       private_key = Rails.application.secrets.oracle_cloud[:private_key]
       public_key  = Rails.application.secrets.oracle_cloud[:public_key]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,11 @@ require "manageiq-providers-oracle_cloud"
 VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = File.join(ManageIQ::Providers::OracleCloud::Engine.root, 'spec/vcr_cassettes')
-  config.define_cassette_placeholder("ocid1.user.oc1..aaaaaaaa") { Rails.application.secrets.oracle_cloud[:user_id] }
-  config.define_cassette_placeholder("ocid1.tenancy.oc1..aaaaaaaa") { Rails.application.secrets.oracle_cloud[:tenant_id] }
+
+  config.define_cassette_placeholder(Rails.application.secrets.oracle_cloud_defaults[:user_id]) do
+    Rails.application.secrets.oracle_cloud[:user_id]
+  end
+  config.define_cassette_placeholder(Rails.application.secrets.oracle_cloud_defaults[:tenant_id]) do
+    Rails.application.secrets.oracle_cloud[:tenant_id]
+  end
 end
