22f3b5d01a942f4b8fc90f794e2dc20d8e85381a
Adam Grare <agrare@redhat.com>
2021-03-01 14:39:43 -0500

    Move default vcr secrets to config/secrets.default.yml

--- spec/factories/ext_management_system.rb
+++ spec/factories/ext_management_system.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   end
 
   factory :ems_oracle_cloud_with_vcr_authentication, :parent => :ems_oracle_cloud do
-    uid_ems { "ocid1.tenancy.oc1..aaaaaaaa" }
+    uid_ems { Rails.application.secrets.oracle_cloud[:tenant_id] }
 
     after(:create) do |ems|
       # These are all bogus values but Rails Secrets won't work because the replaced
@@ -14,24 +14,9 @@ FactoryBot.define do
       # To re-record the cassette replace these values with real ones and add
       # config.define_cassette_placeholder("ocid1.user.oc1..aaaaaaaa") { "real value" }
       # to spec/spec_helper.rb
-      user_id     = "ocid1.user.oc1..aaaaaaaa"
-      private_key = <<~PRIVATE_KEY
-        -----BEGIN RSA PRIVATE KEY-----
-        MIIBOwIBAAJBAMfW33yRX0zpGhFx8kvQcpip1pHG0QJVpUp7ik4W9JLl0PWqSudF
-        eXF0hFAc+Vx9R7ufpxMZ4lp+10WetdObAzsCAwEAAQJBAIw8R1y9DymDst1nHucB
-        AkoLdR2bbXS78WBRTX77MOobynsP5gJehUI8hEOkbzYIJNBlSS/3aFAHBrd72diL
-        ZPECIQDnr3y/pug4gNsoOeDWPGPxmLbd8cK9LS4KA0jeQ2PbfQIhANzPzNaHwZ5R
-        aV/TnK00HpNJQBSo4S4Qbfc6JkZsD2cXAiEAiMI4s/R07S16sBMCGdPJ9wl7ICWe
-        Gwb5PyXTNIe5AQ0CIC5mzJjYdmuamBY3Fdmf9jzlS74LryZK9ZDae2iZFLOJAiA6
-        7KHzcqovm0lETCaEKSMWhKEcIfmz4qbAqxbx+DmAlQ==
-        -----END RSA PRIVATE KEY-----
-      PRIVATE_KEY
-      public_key = <<~PUBLIC_KEY
-        -----BEGIN PUBLIC KEY-----
-        MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMfW33yRX0zpGhFx8kvQcpip1pHG0QJV
-        pUp7ik4W9JLl0PWqSudFeXF0hFAc+Vx9R7ufpxMZ4lp+10WetdObAzsCAwEAAQ==
-        -----END PUBLIC KEY-----
-      PUBLIC_KEY
+      user_id     = Rails.application.secrets.oracle_cloud[:user_id]
+      private_key = Rails.application.secrets.oracle_cloud[:private_key]
+      public_key  = Rails.application.secrets.oracle_cloud[:public_key]
 
       ems.authentications << FactoryBot.create(
         :authentication,
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,14 @@ if ENV['CI']
   SimpleCov.start
 end
 
-Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
-Dir[File.join(__dir__, "support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/shared/**/*.rb")].sort.each { |f| require f }
+Dir[File.join(__dir__, "support/**/*.rb")].sort.each { |f| require f }
 
 require "manageiq-providers-oracle_cloud"
 
 VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = File.join(ManageIQ::Providers::OracleCloud::Engine.root, 'spec/vcr_cassettes')
+  config.define_cassette_placeholder("ocid1.user.oc1..aaaaaaaa") { Rails.application.secrets.oracle_cloud[:user_id] }
+  config.define_cassette_placeholder("ocid1.tenancy.oc1..aaaaaaaa") { Rails.application.secrets.oracle_cloud[:tenant_id] }
 end
