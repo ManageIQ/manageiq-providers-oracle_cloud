ef4a1b5bd5631779ce555d246a9adcb357269a16
Oleg Barenboim <chessbyte@gmail.com>
2021-03-24 16:47:55 -0400

    Merge pull request #19 from agrare/cleanup
    Remove some leftover code from original repo
    
    (cherry picked from commit dc4ca384d60d9d6350a1b2018d98077c26a375ec)

--- app/models/manageiq/providers/oracle_cloud/cloud_manager/vm.rb
+++ app/models/manageiq/providers/oracle_cloud/cloud_manager/vm.rb
@@ -1,19 +1,4 @@
 class ManageIQ::Providers::OracleCloud::CloudManager::Vm < ManageIQ::Providers::CloudManager::Vm
-  include_concern 'Operations'
-
-  def provider_object(connection = nil)
-    connection ||= ext_management_system.connect
-    connection.instance.get(ems_ref)
-  end
-
-  def disconnected
-    false
-  end
-
-  def disconnected?
-    false
-  end
-
   def self.calculate_power_state(raw_power_state)
     case raw_power_state.downcase
     when /running/, /starting/
