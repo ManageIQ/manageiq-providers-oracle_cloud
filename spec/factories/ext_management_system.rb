224220bc190b1d2d43ff391d075c279de9f02718
Adam Grare <adam@grare.com>
2021-08-24 16:55:44 -0400

    Add OKE factories

--- spec/factories/ext_management_system.rb
+++ spec/factories/ext_management_system.rb
@@ -19,4 +19,33 @@ FactoryBot.define do
       )
     end
   end
+
+  factory :ems_oracle_oke, :class => "ManageIQ::Providers::OracleCloud::ContainerManager", :parent => :ems_container do
+    provider_region { "us-ashburn-1" }
+  end
+
+  factory :ems_oracle_oke_with_vcr_authentication, :parent => :ems_oracle_oke do
+    realm { Rails.application.secrets.oracle_oke[:tenant_id] }
+    uid_ems { Rails.application.secrets.oracle_oke[:cluster_id] }
+
+    after(:create) do |ems|
+      user_id     = Rails.application.secrets.oracle_oke[:user_id]
+      private_key = Rails.application.secrets.oracle_oke[:private_key]
+      public_key  = Rails.application.secrets.oracle_oke[:public_key]
+
+      ems.default_endpoint.update!(
+        :hostname          => Rails.application.secrets.oracle_oke[:hostname],
+        :port              => Rails.application.secrets.oracle_oke[:port],
+        :security_protocol => "ssl-without-validation"
+      )
+
+      ems.authentications << FactoryBot.create(
+        :authentication,
+        :authtype   => "bearer",
+        :userid     => user_id,
+        :auth_key   => private_key,
+        :public_key => public_key
+      )
+    end
+  end
 end
