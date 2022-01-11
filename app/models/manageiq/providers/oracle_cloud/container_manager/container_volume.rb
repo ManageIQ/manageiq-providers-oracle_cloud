153a8716d7502608da0da8a311917ac136943eb6
Adam Grare <adam@grare.com>
2022-01-11 09:28:46 -0500

    Don't subclass ContainerVolume
    ContainerVolume cannot be subclassed currently as there are a number of
    places which explicitly look for :type => ContainerVolume

--- app/models/manageiq/providers/oracle_cloud/container_manager/container_volume.rb
+++
@@ -1,4 +0,0 @@
-ManageIQ::Providers::Kubernetes::ContainerManager::ContainerVolume.include(ActsAsStiLeafClass)
-
-class ManageIQ::Providers::OracleCloud::ContainerManager::ContainerVolume < ManageIQ::Providers::Kubernetes::ContainerManager::ContainerVolume
-end
