51d29f57761b7ef59fe6b82267a5dec530adcbdb
Jason Frey <fryguy9@gmail.com>
2025-07-01 15:37:43 -0400

    Switch to use VcrSecrets helper from core

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,11 +12,6 @@ VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = File.join(ManageIQ::Providers::OracleCloud::Engine.root, 'spec/vcr_cassettes')
 
-  secrets = Rails.application.secrets
-  secrets.oracle_cloud.each do |key, val|
-    config.define_cassette_placeholder(secrets.oracle_cloud_defaults[key]) { val }
-  end
-  secrets.oracle_oke.each do |key, val|
-    config.define_cassette_placeholder(secrets.oracle_oke_defaults[key]) { val }
-  end
+  VcrSecrets.define_all_cassette_placeholders(config, :oracle_cloud)
+  VcrSecrets.define_all_cassette_placeholders(config, :oracle_oke)
 end
